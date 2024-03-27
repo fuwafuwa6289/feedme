785641ed20a76148925b786a4dea3bfae45f2965
SoyJuu <pantira151@gmail.com>
2024-03-27 15:09:27 +0700

    ควย
    เาพยเานพาเบพาเ

--- Screen/Post.js
+++ Screen/Post.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import ClassThai from './ClassThai'
 
 function Post({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text onPress={() => navigation.navigate('Home')}>Post Hi!</Text>
-    </View>
+    <ClassThai></ClassThai>
   );
 }
 
