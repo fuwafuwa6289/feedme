b3fd0a2caf6cab0787b10bd2498ba1f855b3fb51
Jutharat Nartsoongnoen <jutharat41242@gmail.com>
2024-03-27 15:24:59 +0700

    Update Post.js

--- Screen/Post.js
+++ Screen/Post.js
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import ClassThai from './ClassThai'
 
 function Post({ navigation }) {
   return (
-<<<<<<< HEAD
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text onPress={() => navigation.navigate('Home')}>Post Hi!</Text>
     </View>
-    // สวย
-=======
-    <ClassThai></ClassThai>
->>>>>>> 785641ed20a76148925b786a4dea3bfae45f2965
+    // สวยมาก
   );
 }
 
