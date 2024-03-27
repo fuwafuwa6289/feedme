627446a5135d38a49a95958b2e23ffff293ab611
Jutharat Nartsoongnoen <jutharat41242@gmail.com>
2024-03-27 15:48:30 +0700

    Update Home.js
    แแแ

--- Screen/Home.js
+++ Screen/Home.js
@@ -45,7 +45,7 @@ const Class = () => {
 
 
         <View style={styles.titleclass}>
-          <Text style={styles.txtclass}>ร้านอาหารใกล้ฉั</Text>
+          <Text style={styles.txtclass}>ร้านอาหารใกล้ฉัน</Text>
         </View>
 
         {/* checkbox */}
