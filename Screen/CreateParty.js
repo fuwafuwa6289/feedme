8e68b10628e297289e8ce24998a68fb568ca53de
Funnyy153 <funnyy153@gmail.com>
2024-04-12 20:18:57 +0700

    เปลี่ยน font , สร้าง party ย้ายหน้า

--- Screen/CreateParty.js
+++ Screen/CreateParty.js
@@ -48,6 +48,7 @@ const CreateParty = ({ route,navigation }) => {
     React.useEffect(() => {
         if (!isFocused) {
             // Reset all state variables to their initial values 
+            setValue(null);
             onChangeText(null);
             onChangeText3(null);
             onChangeText2(null);
@@ -105,7 +106,7 @@ const CreateParty = ({ route,navigation }) => {
     const [open1, setOpen1] = React.useState(false);
     const [value, setValue] = React.useState(null);
     const data = [
-        { label: '1', value: '1' },
+        
         { label: '2', value: '2' },
         { label: '3', value: '3' },
         { label: '4', value: '4' },
@@ -113,6 +114,19 @@ const CreateParty = ({ route,navigation }) => {
         { label: '6', value: '6' },
         { label: '7', value: '7' },
         { label: '8', value: '8' },
+        { label: '9', value: '9' },
+        { label: '10', value: '10' },
+        { label: '11', value: '11' },
+        { label: '12', value: '12' },
+        { label: '13', value: '13' },
+        { label: '14', value: '14' },
+        { label: '15', value: '15' },
+        { label: '16', value: '16' },
+        { label: '17', value: '17' },
+        { label: '18', value: '18' },
+        { label: '19', value: '19' },
+        { label: '20', value: '20' },
+        
     ];
     // const [selectedImage1, setSelectedImage1] = React.useState([]);
     // const [selectedImage2, setSelectedImage2] = React.useState([]);
@@ -180,6 +194,7 @@ const CreateParty = ({ route,navigation }) => {
         setShowModal(true);
         setTimeout(() => {
             setShowModal(false);
+            navigation.navigate('Post');
         }, 3000);
     };
     const formatDateThai = (date) => {
@@ -259,7 +274,7 @@ const CreateParty = ({ route,navigation }) => {
                         <LinearGradient
                             colors={['#FFC0A6', '#FFD4C2']}
                             style={{ flex: 1 }}>
-                            <Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Mitr-Regular', }}>ชื่อปาร์ตี้</Text>
+                            <Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Kanit-Regular', }}>ชื่อปาร์ตี้</Text>
                         </LinearGradient>
                     </View>
                     <TextInput
@@ -273,12 +288,12 @@ const CreateParty = ({ route,navigation }) => {
                     <View style={styles.textdate}>
                     <LinearGradient
                             colors={['#FFE3D7', '#FFF0EA']}
-                            style={{ flex: 1 }}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Mitr-Regular'}}>วันที่</Text>
+                            style={{ flex: 1 }}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Kanit-Regular'}}>วันที่</Text>
                              </LinearGradient></View>
                     <View style={styles.input}>
                         <TouchableOpacity onPress={() => setOpen(true)}>
                             <View>
-                                <Text style={{ color: 'black' ,fontFamily:'Mitr-Regular'}}>{formatDateThai(date1)} </Text>
+                                <Text style={{ color: 'black' ,fontFamily:'Kanit-Regular'}}>{formatDateThai(date1)} </Text>
                             </View>
                         </TouchableOpacity>
                         {open && (
@@ -326,7 +341,7 @@ const CreateParty = ({ route,navigation }) => {
                     </View>
                     
 
-                    <View style={styles.textinput}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Mitr-Regular'}}>สถานที่</Text></View>
+                    <View style={styles.textinput}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Kanit-Regular'}}>สถานที่</Text></View>
                     <TextInput
                         style={styles.input}
                         onChangeText={onChangeText2}
@@ -334,7 +349,7 @@ const CreateParty = ({ route,navigation }) => {
                         placeholder="เลือกสถานที่"
                     />
 
-                    <View style={styles.textinput1}><Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Mitr-Regular' }}>จำนวนคน</Text></View>
+                    <View style={styles.textinput1}><Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Kanit-Regular' }}>จำนวนคน</Text></View>
                     <Dropdown
                         style={styles.dropdown}
                         placeholderStyle={styles.placeholderStyle}
@@ -347,13 +362,13 @@ const CreateParty = ({ route,navigation }) => {
                         labelField="label"
                         valueField="value"
                         placeholder="จำนวนคน"
-                        searchPlaceholder="Search..."
+                        searchPlaceholder="ค้นหา"
                         value={value}
                         onChange={item => {
                             setValue(item.value);
                         }}
                     />
-                    <View style={styles.textinput2}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Mitr-Regular'}}>รายละเอียดเพิ่มเติม</Text></View>
+                    <View style={styles.textinput2}><Text style={{ textAlign: 'center', color: '#FF5C00' ,fontFamily:'Kanit-Regular'}}>รายละเอียดเพิ่มเติม</Text></View>
                     <TextInput
                         style={styles.input1}
                         onChangeText={onChangeText3}
@@ -361,7 +376,7 @@ const CreateParty = ({ route,navigation }) => {
                         placeholder="   -"
                     />
                     
-                    <View style={styles.textinput3}><Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Mitr-Regular' }}>รูปภาพ</Text></View>
+                    <View style={styles.textinput3}><Text style={{ textAlign: 'center', color: '#FF5C00',fontFamily:'Kanit-Regular' }}>รูปภาพ</Text></View>
                   
                     <View style={{alignItems:'center',flexDirection: 'row', justifyContent: 'center', borderWidth: 1, borderColor: '#FF5C00', width: 342, height: 131, bottom: 5, borderRadius: 12, }}>
                         
@@ -416,7 +431,7 @@ data={[selectedImage1, selectedImage2, selectedImage3,selectedImage4,selectedIma
                         <Button
                             title="สร้างปาร์ตี้"
                             titleStyle={{
-                            fontFamily:'Mitr-Regular'
+                            fontFamily:'Kanit-Regular'
                         }}
                             buttonStyle={{
                                 backgroundColor: '#FF8259',
@@ -435,6 +450,7 @@ data={[selectedImage1, selectedImage2, selectedImage3,selectedImage4,selectedIma
                                 create();
                                 handlePopup();
                                 countItems();
+                                
                             }}
                             
                             
@@ -460,7 +476,7 @@ data={[selectedImage1, selectedImage2, selectedImage3,selectedImage4,selectedIma
                                             />
                                         </View>
                                         <Text style={{ alignSelf: 'center', fontSize: 24, fontWeight: 'bold', color: '#E6420C' }}>สร้างปาร์ตี้สำเร็จ</Text>
-
+                                            
                                     </View>
                                 </LinearGradient>
                             </View>
@@ -478,7 +494,7 @@ const styles = StyleSheet.create({
 
 
     text: {
-fontFamily:'Mitr-Regular',
+fontFamily:'Kanit-Regular',
        fontSize: 40,
         marginTop: 90,
         alignSelf: 'center',
@@ -497,7 +513,7 @@ fontFamily:'Mitr-Regular',
         color: 'black',
         borderColor: '#FF5C00',
         display: 'flex',
-        fontFamily:'Mitr-Regular'
+        fontFamily:'Kanit-Regular'
     },inputtime: {
         width: 342,
         height: 44,
@@ -507,7 +523,7 @@ fontFamily:'Mitr-Regular',
         color: 'black',
         borderColor: '#FF5C00',
         display: 'flex',
-        fontFamily:'Mitr-Regular',
+        fontFamily:'Kanit-Regular',
         marginTop:1
     },
     containerinput: {
@@ -549,12 +565,12 @@ fontFamily:'Mitr-Regular',
         borderColor: '#FF5C00',
         borderWidth: 1,
         bottom: 15,
-        fontFamily:'Mitr-Regular'
+        fontFamily:'Kanit-Regular'
     },
     placeholderStyle: {
         fontSize: 15,
         color: '#A8ABAD',
-        fontFamily:'Mitr-Regular'
+        fontFamily:'Kanit-Regular'
     },
     textinput1: {
         height: 20,
@@ -582,7 +598,7 @@ fontFamily:'Mitr-Regular',
         borderColor: '#FF5C00',
         display: 'flex',
         bottom: 28,
-        fontFamily:'Mitr-Regular'
+        fontFamily:'Kanit-Regular'
     }, textinput3: {
         height: 20,
         width: 50,
