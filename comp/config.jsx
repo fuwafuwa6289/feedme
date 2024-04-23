fb3de8a04e44a9939dfaee5089906f56f5c39615
Funnyy153 <funnyy153@gmail.com>
2024-04-23 23:01:05 +0700

    d
    d

--- comp/config.jsx
+++ comp/config.jsx
@@ -3,6 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,4 +25,3 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
 // const analytics = getAnalytics(app);
-
